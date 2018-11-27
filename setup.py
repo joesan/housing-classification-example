49b88f5a283ef12e636d2cefed0da200dcd7174c
codeintheopen@gmail.com <codeintheopen@gmail.com>
2018-11-27 06:06:44 +0100

    restructure project codebase

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Python codebase for the housing classification ML problem',
+    author='Joesan',
+    license='',
+)
